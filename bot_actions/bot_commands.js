f8c531226f40989e668b78360626a9232c782a41
Yaroslav <yroslav070411@gmail.com>
2023-12-10 23:12:10 +0300

    done"

--- bot_actions/bot_commands.js
+++ bot_actions/bot_commands.js
@@ -24,9 +24,9 @@ export const start = async () => {
 
       if (text === "/start") {
         const user = await User.create({ chatId });
-        return await bot.sendMessage(
+        await bot.sendMessage(
           chatId,
-          `Привет! Это бот Нового Года 2024. Ответь правильно на все загадки, напиши своё стихотворение и получи приз. Начало квеста находится в Читальном Зале. Сканируй там QR-код и напиши ответ на загадку в следующем сообщении!`,
+          `Привет! Это бот Нового Года 2024. Ответь правильно на все загадки, напиши своё стихотворение и получи приз. Чтобы начать квест напиши "Начать" или /begin`,
         );
       }
 
@@ -47,22 +47,140 @@ export const start = async () => {
         where: { chatId },
       });
 
-      // ! ЧЗ
-      if ((await user?.current_state) === "reading_room") {
-        if (text.toLowerCase() === "алексей михайлович, стокгольм, киев") {
-          await user.update({ current_state: "search_trans" });
-          return await bot.sendMessage(
+      // Первый вопрос
+      if (
+        (await user?.current_state) === "begin" &&
+        (text.toLowerCase() === "/begin" || text.toLowerCase() === "начать")
+      ) {
+        await user.update({ current_state: "first" });
+        return bot.sendMessage(
+          chatId,
+          "Ответь теперь на вопрос: Куда стремится каждый уважающий себя студент на большой перемене в 12:30?",
+        );
+      }
+
+      if ((await user?.current_state) === "first") {
+        if (text.toLowerCase === "Столовая") {
+          await bot.sendMessage(
             chatId,
-            `Правильно, вот первая строчка стиха:
-          \nУж щиплет шаловливо щеки
-          
-          \nТеперь тебе нужно угадать, где находится следующий этап квеста и написать сюда!         
-          \nРассказ Джеральда Дарелла, в котором главный герой оказался на пересечении двух реальностей. Разграничением между нашей и жуткой реальностями стали зеркала….
-          \nПодсказка:_____ из Л в В
+            `Молодец! Держи строчку: 
+          \nУж щиплет шаловливо щеки`,
+          );
+          await user.update({ current_state: "second" });
+          return bot.sendMessage(
+            chatId,
+            "Вопрос: в истории встречалось много узников, однако самым загадочным по праву считается заключённый времён Людовика XIV по имени *******. Однако историки всегда оспаривали многочисленные мифы, ходящие вокруг личности этого человека. Поднимите и вы завесу тайны.",
+          );
+        }
+        return bot.sendMessage(chatId, answers.wrong_answer);
+      }
+
+      // Вторая
+      if ((await user?.current_state) === "second") {
+        if (text.toLowerCase() === "железная маска") {
+          await bot.sendMessage(
+            chatId,
+            `Молодец! Держи строчку: 
+          \nДекабрьский утренний мороз.`,
+          );
+          await user.update({ current_state: "third" });
+          return bot.sendPhoto(chatId, "https://ibb.co/L68qRgK", {
+            caption: "Ответь на вопрос и получи еще одну строчку!",
+          });
+        }
+        return bot.sendMessage(chatId, answers.wrong_answer);
+      }
+
+      if ((await user?.current_state) === "third") {
+        if (text.toLowerCase() === "мандарин") {
+          await bot.sendMessage(
+            chatId,
+            `Молодец! Держи строчку: 
+          \nИ сессия, грозясь серьезно,`,
+          );
+          await user.update({ current_state: "fourth" });
+          return bot.sendMessage(
+            chatId,
+            `Напиши год рождения человека-аббревиатуры (по первым буквам):
+          \nЖёлтый цитрусовый фрукт
+          \nГород, где умерла Цветаева 
+          \nКруг над головой святого
+          \nНаша ОП
+          \nОстановка поединка у борцов
           `,
           );
         }
-        return await bot.sendMessage(chatId, answers.wrong_answer);
+        return bot.sendMessage(chatId, answers.wrong_answer);
+      }
+
+      if ((await user?.current_state) === "fourth") {
+        if (text === 1870 || text === "1870") {
+          await bot.sendMessage(
+            chatId,
+            `Молодец! Держи строчку: 
+          \nКак на голову снег, студенту валится на нос.`,
+          );
+          await user.update({ current_state: "fifth" });
+          return bot.sendMessage(
+            chatId,
+            `Тут каждый скоро скинет маску,
+Влекомый знанием, оставит шарф, перчатки, может, даже зонт.
+Бродя среди рядов и цифр искомых, 
+Он следующей загадки знак отыщет влет.
+          `,
+          );
+        }
+        return bot.sendMessage(chatId, answers.wrong_answer);
+      }
+
+      if ((await user?.current_state) === "fifth") {
+        if (
+          text.toLowerCase() === "гардероб" ||
+          text.toLowerCase() === "переодевалка" ||
+          text.toLowerCase() === "раздевалка"
+        ) {
+          await bot.sendMessage(
+            chatId,
+            `Молодец! Держи строчку: 
+          \nНо сердце полно уже сладкой зимней сказкой,`,
+          );
+          await user.update({ current_state: "sixth" });
+          return bot.sendPhoto(chatId, "https://ibb.co/L1gMrgs", {
+            caption: "Волк, не тот, кто Волк, а тот, кто в перерыв бежит work  в ….",
+          });
+        }
+        return bot.sendMessage(chatId, answers.wrong_answer);
+      }
+
+      if ((await user?.current_state) === "sixth") {
+        if (text.toLowerCase() === "коворк") {
+          await bot.sendMessage(
+            chatId,
+            `Молодец! Держи строчку: 
+          \nИ словно бой курантов песнь его.`,
+          );
+          await user.update({ current_state: "seventh" });
+          return bot.sendPhoto(chatId, "https://ibb.co/Yb1yqrj", {
+            caption:
+              "Задание: отгадать слово с помощью указанных в тексте цифр. Каждой цифре соответствует своя буква на циферблате.\n",
+          });
+        }
+        return bot.sendMessage(chatId, answers.wrong_answer);
+      }
+
+      if ((await user?.current_state) === "seventh") {
+        if (text.toLowerCase() === "дедлайн" || text.toLowerCase() === "дэдлайн") {
+          await bot.sendMessage(
+            chatId,
+            `Молодец! Держи строчку: 
+          \nПод звон лихой кружится все в веселом танце,`,
+          );
+          await user.update({ current_state: "eidth" });
+          return bot.sendPhoto(chatId, "https://ibb.co/Yb1yqrj", {
+            caption:
+              "Узнайте любимых преподавателей в образах дедов Морозов. Ответом станет слово состоящее из 1 буквы фамилии первого «деда», 4 и 9 букв фамилии третьего, 7-ой  третьего и 2 -ой четвертого.",
+          });
+        }
       }
 
       // ! Переход
--- models/models.js
+++ models/models.js
@@ -4,5 +4,5 @@ import { DataTypes } from "sequelize";
 export const User = sequilize.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true },
   chatId: { type: DataTypes.STRING, unique: true },
-  current_state: { type: DataTypes.STRING, defaultValue: "reading_room" },
+  current_state: { type: DataTypes.STRING, defaultValue: "begin" },
 });
